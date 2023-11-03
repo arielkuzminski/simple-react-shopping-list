c3928217136782460bd3c95d505e9b319f6d17d5
Ariel Kuzminski <ariel.kuzminski@gmail.com>
2023-11-03 15:46:58 +0100

    Add container wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex } from "@chakra-ui/react";
 import "./App.css";
 import Footer from "./components/Footer";
 import List from "./components/List";
@@ -9,20 +9,22 @@ function App() {
   const { items, handleAddItem, editItem, clearItems } = useItems();
 
   return (
-    <Flex
-      minWidth={"max-content"}
-      alignItems={"center"}
-      gap={2}
-      direction={"column"}
-      height={"100dvh"}
-      maxW={"100dvw"}
-    >
-      <Navbar onClearItems={clearItems}></Navbar>
-      <Box flex="1" overflow={"auto"} width={"full"} padding={2}>
-        <List onEditItem={editItem} items={items}></List>
-      </Box>
-      <Footer onAddItem={handleAddItem}></Footer>
-    </Flex>
+    <Container>
+      <Flex
+        minWidth={"max-content"}
+        alignItems={"center"}
+        gap={2}
+        direction={"column"}
+        height={"100dvh"}
+        maxW={"100dvw"}
+      >
+        <Navbar onClearItems={clearItems}></Navbar>
+        <Box flex="1" overflow={"auto"} width={"full"} padding={2}>
+          <List onEditItem={editItem} items={items}></List>
+        </Box>
+        <Footer onAddItem={handleAddItem}></Footer>
+      </Flex>
+    </Container>
   );
 }
 
