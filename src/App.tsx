e596bd1d50aa412f0007f3a3aee64a943b532adb
Wotan91 <ariel.kuzminski@gmail.com>
2023-11-05 00:52:43 +0100

    Feature: Sort by name, date, completion

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,31 @@ import Footer from "./components/Footer";
 import List from "./components/List";
 import Navbar from "./components/Navbar";
 import useItems from "./hooks/useItems";
+import { useState } from "react";
 
 function App() {
   const { items, handleAddItem, editItem, clearItems } = useItems();
+  const [sorting, setSorting] = useState("byDate");
+
+  const handleSorting = () => {
+    if (sorting === "byDate") {
+      return items.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+    }
+
+    if (sorting === "byName") {
+      return items.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (sorting === "byCompleted") {
+      return items.sort((a, b) => +a.completed - +b.completed);
+    }
+
+    return items;
+  };
+
+  const sortedItems = handleSorting();
 
   return (
     <Container>
@@ -18,9 +40,14 @@ function App() {
         height={"100dvh"}
         maxW={"100dvw"}
       >
-        <Navbar onClearItems={clearItems} count={items.length} completed={items.filter(item => item.completed).length}></Navbar>
+        <Navbar
+          onSort={setSorting}
+          onClearItems={clearItems}
+          count={items.length}
+          completed={items.filter((item) => item.completed).length}
+        ></Navbar>
         <Box flex="1" overflow={"auto"} width={"full"} padding={2}>
-          <List onEditItem={editItem} items={items}></List>
+          <List onEditItem={editItem} items={sortedItems}></List>
         </Box>
         <Footer onAddItem={handleAddItem}></Footer>
       </Flex>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,76 @@
-import { Button, useColorMode, Flex, Spacer } from "@chakra-ui/react";
+import {
+  Button,
+  useColorMode,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import { BsTrash, BsSun, BsMoon } from "react-icons/bs";
 
 interface Props {
   count: number;
   completed: number;
   onClearItems: () => void;
+  onSort: (id: string) => void;
 }
 
-const Navbar = ({ onClearItems, count, completed }: Props) => {
+const sorting = [
+  {
+    id: "byDate",
+    title: "Creation date",
+  },
+  {
+    id: "byName",
+    title: "Name",
+  },
+  {
+    id: "byCompleted",
+    title: "Completion",
+  },
+];
+
+const Navbar = ({ onClearItems, onSort, count, completed }: Props) => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const handleChange = (id: string) => {
+    onSort(id);
+  };
   return (
-    <Flex minWidth="max-content" alignItems="center" gap="2" width={"full"}>
-      <Button onClick={toggleColorMode} margin={2}>
+    <Flex
+      minWidth="max-content"
+      alignItems="center"
+      gap="2"
+      width={"full"}
+      marginTop={2}
+    >
+      <Button onClick={toggleColorMode}>
         {colorMode === "light" ? <BsMoon /> : <BsSun />}
       </Button>
-      <Spacer />
+      <Menu>
+        <MenuButton as={Button}>Sort by:</MenuButton>
+        <MenuList>
+          {sorting.map((menuItem) => (
+            <MenuItem
+              key={menuItem.id}
+              onClick={() => handleChange(menuItem.id)}
+            >
+              {menuItem.title}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
 
-      {count > 0 && completed > 0 && <Button
-        rightIcon={<BsTrash />}
-        colorScheme="blue"
-        variant="outline"
-        onClick={onClearItems}
-        margin={2}
-      >
-        Clear completed
-      </Button>}
+      {count > 0 && completed > 0 && (
+        <Button
+          rightIcon={<BsTrash />}
+          colorScheme="blue"
+          variant="outline"
+          onClick={onClearItems}
+        >
+          Clear completed
+        </Button>
+      )}
     </Flex>
   );
 };
